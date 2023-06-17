6faa83a46d5d7a8efbbb443593ece93c72e0c850
Life is squid <rredzer000@gmail.com>
2023-06-17 22:31:09 +0100

    The project updated from .net 6 to .net 7 Added Identity. Migrated to DB. Added controller handling login. [Not completely finished]

--- API/Startup.cs
+++ API/Startup.cs
@@ -24,19 +24,19 @@ namespace API
 {
     public class Startup
     {
-        private readonly IConfiguration _ñonfiguration;
+        private readonly IConfiguration _configuration;
 
         public Startup(IConfiguration configuration)
         {
-            _ñonfiguration = configuration;
+            _configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
-            services.AddApplicationServices(_ñonfiguration);
+            services.AddApplicationServices(_configuration);
+            services.AddIdentityServices(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
